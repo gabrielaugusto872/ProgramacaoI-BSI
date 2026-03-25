a7f27b460874430a9d0a924e9a93f3f562d15840
Gabriel873 <gabrielaugusto873@yahoo.com.br>
2026-03-25 09:26:47 -0300

    teste

--- aulas/aula01/hello-world/primeiroPrograma.java
+++ aulas/aula01/hello-world/primeiroPrograma.java
@@ -1,6 +1,18 @@
 public class primeiroPrograma{
    public static void main(String []args){
-      System.out.println("Hello word");
+      byte idade = 23;
+      short disciplinas;
+      int filhos;
+      long contas;
+      
+      disciplinas = 5;
+      filhos = 2;
+      contas = 4;
+      
+      System.out.println("Eu tenho " + idade + " anos.");
+      System.out.println("Estou cursando " + disciplinas + " disciplinas.");
+      System.out.println("Tenho " + filhos + " filho(s).");
+      System.out.println("E possuo " + contas + " conta(s) de email.");
    }
 }
 
