3e56a6f3e9b0d80d8cfd101fad736b3b04148301
Gabriel873 <gabrielaugusto873@yahoo.com.br>
2026-04-15 10:11:47 -0300

    atualiza exercico 12

--- Listas/Estrutura Condicional 2 - Gabriel Augusto/src/GabrielAugusto12.java
+++ Listas/Estrutura Condicional 2 - Gabriel Augusto/src/GabrielAugusto12.java
@@ -4,26 +4,41 @@ public class GabrielAugusto12 {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
 
-        String anoAtual = "      ", nascimento = "      ", voto;
+        boolean teste = false;
+        int tamanho;
+        String dataVotacao, nascimento, voto;
 
+        System.out.printf("\nFormato necessário para entrada de datas:\n");
+        System.out.printf("DD/MM/AAAA\n");
 
-        while (anoAtual.charAt(2) != '/' || anoAtual.charAt(5) != '/'){
-            System.out.printf("Formato necessário para entrada de datas:\n");
-            System.out.printf("DD/MM/AAAA\n");
+        while (!teste){
+            System.out.printf("\nDigite a data de votação: ");
+            dataVotacao = teclado.next();
 
-            System.out.printf("Digite o ano Atual: ");
-            anoAtual = teclado.next();
-            teclado.next();
-            if(anoAtual)
+            tamanho = dataVotacao.length();
 
-            while (nascimento.charAt(2) != '/' || nascimento.charAt(5) != '/'){
-                System.out.printf("Formato necessário para entrada de datas:\n");
-                System.out.printf("DD/MM/AAAA\n");
-                System.out.printf("Digite a data de nascimento: ");
-                nascimento = teclado.next();
-                teclado.next();
+            if(tamanho < 10 || dataVotacao.charAt(2) != '/' || dataVotacao.charAt(5) != '/'){
+                System.out.printf("Formato da data inválido!\n");
+            } else {
+                teste = true;
             }
-
         }
+
+        teste = false;
+
+        while (!teste){
+            System.out.printf("\nDigite a data de nascimento do eleitor: ");
+            nascimento = teclado.next();
+
+            tamanho = nascimento.length();
+            
+            if(tamanho < 101 || nascimento.charAt(2) != '/' || nascimento.charAt(5) != '/'){
+                System.out.printf("Formato da data inválido!\n");
+            } else {
+                teste = true;
+            }
+        }
+
+        
     }
 }
