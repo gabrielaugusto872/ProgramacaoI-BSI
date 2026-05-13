f01c152e023614bc659f5bef7e61178da104fdf6
Gabriel873 <gabrielaugusto873@yahoo.com.br>
2026-05-13 10:34:07 -0300

    Relogio Digital

--- aulas/estruturaRepeticao/src/Main.java
+++ aulas/estruturaRepeticao/src/Main.java
@@ -1,40 +1,44 @@
 public class Main {
     public static void main(String[] args) {
         String periodo;
+        for(int l=0;l<2;l++){
+            for(int i=0;i<12;i++){
+                for(int j=0;j<60;j++) {
+                    for(int k=0;k<60;k++){
+                        if(l==0){
+                            periodo = "AM";
+                        } else {
+                            periodo = "PM";
+                        }
+                        if (i == 0){
+                            System.out.printf("12:");
+                        } else if (i < 10) {
+                            System.out.printf("0%d:", i);
+                        } else {
+                            System.out.printf("%d:", i);
+                        }
 
-        for(int i=0;i<24;i++){
-            for(int j=0;j<60;j++) {
-                for(int k=0;k<60;k++){
-                    if(i<12){
-                        periodo = "AM";
-                    } else {
-                        periodo = "PM";
-                    }
-                    if (i < 10) {
-                        System.out.printf("0%d:", i);
-                    } else {
-                        System.out.printf("%d:", i);
-                    }
+                        if (j < 10) {
+                            System.out.printf("0%d:", j);
+                        } else {
+                            System.out.printf("%d:", j);
+                        }
 
-                    if (j < 10) {
-                        System.out.printf("0%d:", j);
-                    } else {
-                        System.out.printf("%d:", j);
-                    }
+                        if (k < 10) {
+                            System.out.printf("0%d %s\n", k, periodo);
+                        } else {
+                            System.out.printf("%d %s\n", k, periodo);
+                        }
 
-                    if (k < 10) {
-                        System.out.printf("0%d %s\n", k, periodo);
-                    } else {
-                        System.out.printf("%d %s\n", k, periodo);
-                    }
-
-                    try{
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e){
-                        e.printStackTrace();
+                        try{
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
         }
+
     }
 }
